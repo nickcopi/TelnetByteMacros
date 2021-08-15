9ec06542231a81332432ce4740c0811f072ce856
nick <nick@localhost.localdomain>
2021-08-15 12:11:42 -0400

    Fix wrapper to actually use tcp proxy.

--- bytemacroclient/wrapper.js
+++ bytemacroclient/wrapper.js
@@ -1,5 +1,6 @@
 const {spawn} = require('child_process');
 const child_process = require('child_process');
+const net = require('net');
 
 const IAC = 255;
 const BM = 19;
@@ -9,44 +10,50 @@ const WONT = 252;
 const PORT = 18492;
 
 const createServer = ()=>{
-	const server =  net.createServer(socket=>{
-		const remoteSocket = net.createConnection(Number(port),process.argv[2], () => {
-			remoteSocket.on('data',data=>{
-				data = warpData(data);
-				if(!socket.write(data))
-					remoteSocket.pause();
-			});
-			remoteSocket.on('drain',()=>{
-				socket.resume();
-			});
-			remoteSocket.on('close',()=>{
-				socket.end();
-			});
-			socket.on('data',data=>{
-				data = warpData(data);
-				if(!remoteSocket.write(data))
-					socket.pause();
+	return new Promise((resolve,reject)=>{
+		const server =  net.createServer(socket=>{
+			const remoteSocket = net.createConnection(23,process.argv[2], () => {
+				remoteSocket.on('data',data=>{
+					//data = warpData(data);
+					if(!socket.write(data))
+						remoteSocket.pause();
+				});
+				remoteSocket.on('drain',()=>{
+					socket.resume();
+				});
+				remoteSocket.on('close',()=>{
+					socket.end();
+				});
+				socket.on('data',data=>{
+					data = warpData(data);
+					if(!remoteSocket.write(data))
+						socket.pause();
+
+				});
+				socket.on('drain',()=>{
+					remoteSocket.resume();
+				});
+				socket.on('close',()=>{
+					remoteSocket.end();
+				});
 
 			});
-			socket.on('drain',()=>{
-				remoteSocket.resume();
-			});
-			socket.on('close',()=>{
-				remoteSocket.end();
-			});
-
+			
+		}); 
+		server.on('error',e=>{
+			console.error(e);
 		});
-		
-	}); 
-	server.on('error',e=>{
-		console.error(e);
+		server.listen(PORT,() => {
+			console.log('Started tcp proxy on', server.address());
+			resolve(server);
+		});
+
 	});
-	return server;
 }
 
 
 const makeTelnet = ()=>{
-	const telnet = spawn('telnet',['localhost']);
+	const telnet = spawn('telnet',['localhost',`${PORT}`]);
 	telnet.stdout.on('data',data=>{
 		console.log(data.toString());
 	});
@@ -66,6 +73,7 @@ const warpData = data=>{
 }
 
 const init = async()=>{
+	const server = await createServer();
 	const telnet = makeTelnet();
 	process.stdin.on('data',data=>{
 		data = warpData(data);
